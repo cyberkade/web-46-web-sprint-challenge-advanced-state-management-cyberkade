1bf1891f5d1921414af3591a740b1645c1008b6f
Kade Griffith <kadeg95@gmail.com>
2021-09-10 09:08:14 -0600

    set up a store with a reducer and applied middleware in the process. Connected actions to reducers and used redux-thunk to create a fetchSmurfs action that gets smurfs from an API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import { connect } from 'react-redux';
+import { fetchSmurfs } from "./actions";
 
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
@@ -9,15 +11,17 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
-class App extends Component {
-  componentDidMount() {
-    axios.get('http://localhost:3333/smurfs')
-    .then(res => console.log(res))
-    .catch(err => console.log('Axios Error', err));
-  }
+const App = (props) => {
 
-  render() {
-    return (
+  useEffect(() => {
+    props.fetchSmurfs();
+  }, []);
+    // axios.get('http://localhost:3333/smurfs')
+    // .then(res => console.log(res))
+    // .catch(err => console.log('Axios Error', err));
+
+
+  return(
       <div className="App">
         <Header />
 
@@ -26,11 +30,10 @@ class App extends Component {
           <AddForm/>
         </main>
       </div>
-    );
-  }
+  );
 }
 
-export default App;
+export default connect(null, {fetchSmurfs})(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
